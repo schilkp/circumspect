39608cc3fb3b0d07b19774b94beed5c35af0624c
schilkp <schilk.philipp@gmail.com>
2025-08-17 11:48:19 +0200

    fix(tests): Correct badly-worded comment.

--- test/dpihdr_match/main.c
+++ test/dpihdr_match/main.c
@@ -2,7 +2,7 @@
 
 // Include both headers to detect inconsistent prototypes.
 // Multiple (compatible) prototypes are allowed in C, but mismatching
-// prototypes cause GCC to act-up.
+// prototypes cause GCC to error-out.
 #include "dpi_hdr_cbindgen.h"
 #include "dpi_hdr_verilator.h"
 
